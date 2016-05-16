2e315346af983091f942874d25191deb4d3ff529
rpopescu <popescu.roxana.andreea@gmail.com>
2016-05-16 18:34:41 +0300

    menu routing

--- www/app/app.js
+++ www/app/app.js
@@ -15,12 +15,81 @@ angular.module("eliteApp",["ionic"])
 		$stateProvider
 
 			.state('home',{
+				abstract: true,
 				url: '/home',
 				templateUrl: 'app/home/home.html'
 			})
+
+			.state('home.leagues',{
+				url:'/leagues',
+				views:{
+					"tab-leagues":{
+						templateUrl: 'app/home/leagues.html'
+				}
+			}
+			})
+
+			.state('home.myteams',{
+				url:'/myteams',
+				views:{
+					'tab-myteams':{
+						templateUrl: 'app/home/myteams.html'
+					}
+				}
+			})
+
 			.state('app',{
+				abstract: true,
 				url:'/app',
 				templateUrl:'app/layout/menu-layout.html'
-			});
-		$urlRouterProvider.otherwise('/app');
+			})
+			.state('app.teams',{
+				url:'/teams',
+				views:{
+					'main-content':{
+						templateUrl:'app/teams/teams.html'
+					}
+				}
+			})
+			.state('app.team-details',{
+				url:'/teams/:id',
+				views:{
+					'main-content':{
+						templateUrl:'app/teams/team-detail.html'
+					}
+				}
+			})
+			.state('app.game',{
+				url:'/game/:id',
+				views:{
+					'main-content':{
+						templateUrl: 'app/game/game.html'
+					}
+				}
+			})
+			.state('app.standings',{
+				url:'/standings',
+				views:{
+					'main-content':{
+						templateUrl: 'app/standings/standings.html'
+					}
+				}
+			})
+			.state('app.locations',{
+				url:'/locations',
+				views:{
+					'main-content':{
+						templateUrl: 'app/locations/locations.html'
+					}
+				}
+			})
+			.state('app.rules',{
+				url:'/rules',
+				views:{
+					'main-content':{
+						templateUrl:'app/rules/rules.html'
+					}
+				}
+			})
+		$urlRouterProvider.otherwise('/home/teams');
 	})
