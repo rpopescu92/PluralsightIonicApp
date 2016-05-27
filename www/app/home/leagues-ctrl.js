bbc21633aa19f013d767c4bbcfe9f7f1b2833249
rpopescu <popescu.roxana.andreea@gmail.com>
2016-05-27 15:37:46 +0300

    standings list

--- www/app/home/leagues-ctrl.js
+++ www/app/home/leagues-ctrl.js
@@ -1,24 +1,22 @@
 /**
  * Created by roxanap on 25.05.2016.
  */
-(function(){
-  'use stricht';
+(function () {
+  'use strict';
 
-  angular.module('eliteApp').controller('LeaguesCtrl',['$state','eliteApi', LeaguesCtrl]);
+  angular.module('eliteApp').controller('LeaguesCtrl', ['$state', 'eliteApi', LeaguesCtrl]);
 
-  function LeaguesCtrl($state, eliteApi){
-
+  function LeaguesCtrl($state, eliteApi) {
     var vm = this;
-    var leagues = eliteApi.getLeagues();
-    var leagueData = eliteApi.getLeagueData();
 
-    console.log(leagues);
-    vm.leagues = leagues;
+    var data = eliteApi.getLeagues();
 
-    vm.selectLeague = function(leagueId){
+    console.log(data);
+    vm.leagues = data;
+
+    vm.selectLeague = function(id){
       $state.go("app.teams");
-
-    };
+    }
 
   };
 })();
